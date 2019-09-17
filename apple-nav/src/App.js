7af3b1269325557261e8536dbc84950a6545b9b0
Manju KC <kcmanjuthapa@gmail.com>
2019-09-17 15:17:27 -0400

    created NavWrapper component and import to the app.js

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NavWrapper from "./Component/NavWrapper"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-     <h1>Hello World!</h1>
-      </div>
+     <NavWrapper/>
     );
   }
 }
