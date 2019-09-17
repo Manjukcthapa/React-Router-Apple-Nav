d72df17db520cf2c65d11608ea88d1074d859a50
Manju KC <kcmanjuthapa@gmail.com>
2019-09-17 17:53:39 -0400

    created SubItem component and import to the SubLinks.js

--- apple-nav/src/Component/Menu/SubItem.js
+++ apple-nav/src/Component/Menu/SubItem.js
@@ -1,2 +1,15 @@
-
+import React from 'react';
+ 
+const SubItem = (props) => {
+    return (
+        <div>
+            {props.subLinks.map((_subItem) => (
+            <div>
+                <img src={subItem.img} alt={subItem.name}/>
+                <h3>{subItem.name}</h3>
+            </div>
+            ))}
+        </div>
+    )
+}
 
---
+++ apple-nav/src/Component/Menu/Sublinks.js
@@ -0,0 +1,11 @@
+import React from "react";
+import SubLinks from './SubItem'
+
+const SubItem = () => {
+
+	return(
+		<div>
+        <SubItem/>
+		</div>
+	)
+}
