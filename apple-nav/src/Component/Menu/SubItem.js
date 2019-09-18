d1b959f6da94f56ce87df72c0b2b86c558b4796e
Manju KC <kcmanjuthapa@gmail.com>
2019-09-17 21:44:18 -0400

    changed some styled

--- apple-nav/src/Component/Menu/SubItem.js
+++ apple-nav/src/Component/Menu/SubItem.js
@@ -4,25 +4,36 @@ import styled from 'styled-components'
 const Img = styled.img`
 height:100px;
 width:100px;
+margin-left:29px;
 `
 const Div = styled.div`
 display: flex;
 flex-direction: row;
- 
+border:10px solid #C0C0C0;
+background-color:#C0C0C0;
+height:240px;
 `     
+const Li = styled.li`
+list-style-type: none;
+margin-left:30px;
+font-size:20px;
+`
+const Item = styled.div`
+margin-left:10px;
+`
 
 
-import { Link } from 'react-router-dom';
+
 
 const SubItems = (props) => {
 	return (
 		<Div className="sub-item">
 			{props.subLinks.map((subItem) => (
                 <div>
-				<Link to="/" key={subItem.id}>
+                    <Item>
 					<Img src={subItem.img} alt={subItem.name} />
-					<h3>{subItem.name}</h3>
-				</Link>
+					<Li>{subItem.name}</Li>
+                    </Item>
                 </div>
 			))}
 		</Div>
--- apple-nav/src/Component/NavList.js
+++ apple-nav/src/Component/NavList.js
@@ -1,34 +1,41 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import styled from 'styled-components';
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const Div = styled.div`
-display: flex;
-flex-direction: row;
-justify-content: center;
-background-color: green;
- 
-` 
-const H2 = styled.h2`
-text-decoration: none;
-padding-left:100px;
+  display: flex;
+  flex-direction: row;
+  background-color: black;
+  height: 100px;
+  border: 10px solid black;
+`;
+const H3 = styled.h3`
+  padding-left: 100px;
+  color: white;
+  text-decoration: none;
+  margin-top: 40px;
+`;
+const Img = styled.img`
+ margin-top:15px;
+ height:50px;
+ width:50px;
+ margin-left:10px;
 
+`;
 
-`
-
-
-
-
-const NavItem = (props) => {
-	return (
-		<Div className="nav-item">
-			{props.links.map((item) => (
-				<Link  className ="Link" to={`/sub-links/${item.name}`}>
-					<H2>{item.name}</H2>
-				</Link>
-			))}
-		</Div>
-	);
+const NavItem = props => {
+  return (
+    <div>
+      <Div className="nav-item">
+        <Img src="https://image.flaticon.com/icons/svg/179/179309.svg" />
+        {props.links.map(item => (
+          <Link className="Link" to={`/sub-links/${item.name}`}>
+            <H3>{item.name}</H3>
+          </Link>
+        ))}
+      </Div>
+    </div>
+  );
 };
 
 export default NavItem;
